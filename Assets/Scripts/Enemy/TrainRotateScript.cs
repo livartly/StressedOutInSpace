e6163b07d0fa1bb1e5903346eabbbe85553861e4
Liv Artly <37487751+livartly@users.noreply.github.com>
2018-11-15 13:54:43 -0600

    Train Script and Sprite Addition
    Train rotates around z axis of Metropopolopolis and runs through idle animation. A half circle collider needs to be applied.

---
+++ Assets/Scripts/Enemy/TrainRotateScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainRotateScript : MonoBehaviour
+{
+    public GameObject planet;
+
+    public Vector3 axis;
+
+    public float speed = 10;
+
+    void Update()
+    {
+        transform.RotateAround(planet.transform.position, axis, speed);
+    }
+}
