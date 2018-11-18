ffaf400efc46d19cb91e0206e98e36d18d9ba657
Liv Artly <37487751+livartly@users.noreply.github.com>
2018-11-17 21:08:54 -0600

    Addition of GameBoyObject script, gameboy sprite, and GameBoyCollider in scene

---
+++ Assets/Scripts/PickUps/GameBoyObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBoyObject : MonoBehaviour
+{
+
+    //object declaration
+    PickupObject gameboy = new PickupObject("Gameboy", "positive");
+
+    public AnxietyBarScript anxietyLvl;
+
+    void OnTriggerEnter(Collider coll)
+    {
+        //if Player
+        if (coll.tag == "Player")
+        {
+            //Relieve Anxiety
+            anxietyLvl.SubtractAnxitey();
+            //Delete
+            Destroy(gameObject);
+        }
+    }
+
+}
