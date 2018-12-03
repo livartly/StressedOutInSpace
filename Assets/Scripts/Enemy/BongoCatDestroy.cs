275e5da19fd9f25bca958e2a8d1cb78e28fb929f
Liv Artly <37487751+livartly@users.noreply.github.com>
2018-12-02 21:54:11 -0600

    BongoCat Destroy script
    Player can now destroy BongoCat by jumping on top of him and colliding with top collider

---
+++ Assets/Scripts/Enemy/BongoCatDestroy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BongoCatDestroy : MonoBehaviour {
+
+    public GameObject BongoCat;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            //Destroy BongoCat
+            Destroy(BongoCat);
+            Destroy(gameObject);
+        }
+    }
+}
